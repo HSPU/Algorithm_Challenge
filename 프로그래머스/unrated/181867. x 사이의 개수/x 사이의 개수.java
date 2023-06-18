ccf759b8b8bf738ad60b78475b3ac08236cbf07e
HSPU <95858935+HSPU@users.noreply.github.com>
2023-06-18 17:23:03 +0900

    [unrated] Title: x 사이의 개수, Time: 5.04 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(String myString) {
+        String[] str = myString.split("x", -1);
+        int[] answer = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] != "x") {
+                answer[i] = str[i].length();
+            }
+        }
+        return answer;
+    }
+}

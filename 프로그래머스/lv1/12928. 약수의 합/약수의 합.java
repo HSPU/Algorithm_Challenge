381f77d8976e2254550b9e432c57a3203996f3ff
HSPU <95858935+HSPU@users.noreply.github.com>
2023-06-18 17:29:12 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int num = n;
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}

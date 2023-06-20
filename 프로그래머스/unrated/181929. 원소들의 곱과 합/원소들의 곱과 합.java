964e728a162e70d0f2c829e00940e4108206b289
HSPU <95858935+HSPU@users.noreply.github.com>
2023-06-20 17:07:49 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int[] num_list) {
         int multi = 1;
-        for (int i = 0; i < num_list.length; i++) multi *= num_list[i];
         int sum = 0;
-        for (int i = 0; i < num_list.length; i++) sum += num_list[i];
-        
+        for (int i = 0; i < num_list.length; i++) {
+            multi *= num_list[i];
+            sum += num_list[i];
+        }
         if (multi < sum * sum) return 1;
         else return 0;
     }

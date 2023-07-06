46a55f3663364718835f324e437294cc4e42fbb2
HSPU <95858935+HSPU@users.noreply.github.com>
2023-07-06 16:14:54 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,12 +1,8 @@
 class Solution {
     public double solution(int[] numbers) {
-        double answer = 0;
-        double sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];    
-        }
-        double average = sum / numbers.length;
-        answer = average;
-        return answer;
+        float sum = 0;
+        for (int i = 0; i < numbers.length; i++) sum += numbers[i];    
+        float average = sum / numbers.length;
+        return average;
     }
 }

3abc2afdea8b7a69880d051a849b93d88cc216e6
HSPU <95858935+HSPU@users.noreply.github.com>
2023-07-06 16:09:30 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int idx = 0;
+        for (int i = num_list.length - 1; i >= 0; i--) answer[idx++] = num_list[i];
+        return answer;
+    }
+}

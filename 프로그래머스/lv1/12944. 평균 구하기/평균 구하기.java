710c777b871a3bcf1f09178ae86a94f209be55b8
HSPU <95858935+HSPU@users.noreply.github.com>
2023-07-11 16:14:24 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public double solution(int[] arr) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) sum += arr[i];
+        return sum / arr.length;
+    }
+}

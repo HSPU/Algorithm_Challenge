73ceb6f567d4b265dab6d9ada1274196ce83eb70
HSPU <95858935+HSPU@users.noreply.github.com>
2023-07-12 11:16:08 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int cnt = 0;
+        for (int i = 0; i < array.length; i++) if (array[i] == n) cnt++;
+        return cnt;
+    }
+}

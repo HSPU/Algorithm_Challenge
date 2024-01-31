cedb1c230226d61a07c31a75b99cd974d0193c6d
HSPU <95858935+HSPU@users.noreply.github.com>
2024-02-01 00:35:11 +0900

    [level 2] Title: 기능개발, Time: 0.15 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -9,24 +9,9 @@ class Solution {
         for (int i = 0; i < progresses.length; i++) {
             int day = (100 - progresses[i]) / speeds[i];
             if ((100 - progresses[i]) % speeds[i] != 0) day += 1;
-            // 나머지 있으면 +1
             q.offer(day);
         }
-            
-//         while (!q.isEmpty()) {
-//             int current = q.poll();
-//             cnt++;
-//             if (!q.isEmpty()) {
-//                 if (current >= q.peek()) {
-//                     cnt++;
-//                     answer.add(cnt);
-//                 } else {
-//                     cnt = 1;
-//                     answer.add(cnt);
-//                 }
-//             } 
-//         }
-        
+
         while (!q.isEmpty()) {
             int current = q.poll();
             cnt = 1;
@@ -38,11 +23,10 @@ class Solution {
             answer.add(cnt);
         }
         
+        int idx = 0;
         int[] arr = new int[answer.size()];
-        // for (int i : answer) arr[i] = answer.get(i);
-        for (int i = 0; i < answer.size(); i++) arr[i] = answer.get(i);
+        for (int i : answer) arr[idx++] = i;
         
-        System.out.println(answer);
         return arr;
     }
 }

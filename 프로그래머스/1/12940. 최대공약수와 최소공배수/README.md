c903da04a157a63299df7c24310ddb641b95053a
HSPU <95858935+HSPU@users.noreply.github.com>
2024-03-11 11:09:29 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최대공약수와 최소공배수 - 12940 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940#) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 0.07 ms
+메모리: 74.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 11일 10:47:51
+2024년 03월 11일 11:09:28
 
 ### 문제 설명
 
--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -2,16 +2,14 @@ class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
         // 최대공약수
-        // if (m % n == 0 || n % m == 0) answer[0] = n;
-        // else answer[0] = m % n;
-        int gcd = 1;
+        int gcd = 0;
         for (int i = 1; i <= n && i <= m; i++) {
             if (n % i == 0 && m % i == 0) gcd = i;
         }
         answer[0] = gcd;
         
         // 최소공배수
-        answer[1] = m * n / answer[0];
+        answer[1] = m * n / gcd;
         return answer;
     }
 }

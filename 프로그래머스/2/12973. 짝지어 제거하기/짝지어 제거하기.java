5a2296f6b66ec49b1720185294e7f236c1db9cfc
HSPU <95858935+HSPU@users.noreply.github.com>
2024-03-13 23:14:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 49.97 ms, Memory: 59.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (stack.empty()) stack.push(c);
+            else {
+                if (stack.peek() == c) stack.pop();
+                else stack.push(c);
+            }
+        }
+        int answer = (stack.size() == 0) ? 1 : 0;
+
+        return answer;
+    }
+}

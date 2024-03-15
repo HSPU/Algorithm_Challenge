7dbb2fa1c120330f7c23c2bba84d5c98f9497fdb
HSPU <95858935+HSPU@users.noreply.github.com>
2024-03-15 11:21:57 +0900

    [level 2] Title: 택배상자, Time: 49.84 ms, Memory: 141 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        int cnt = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        int idx = 0;
+        for (int i = 1; i <= order.length; i++) {
+            if (order[idx] != i) stack.push(i);
+            else {
+                idx++;
+                cnt++;
+            }
+            // System.out.println(stack);
+            while (!stack.isEmpty()) {
+                if (stack.peek() == order[idx]) {
+                    stack.pop();
+                    cnt++;
+                } else break;
+                idx++;
+            }
+            
+        }
+        
+        return cnt;
+    }
+}

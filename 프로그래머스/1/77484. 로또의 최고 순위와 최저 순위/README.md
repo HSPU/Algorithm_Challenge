e7ebfab05ef131a3dc8fbc057bb429688d39b94e
HSPU <95858935+HSPU@users.noreply.github.com>
2024-04-01 11:42:31 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484#fn1) 
 
 ### 성능 요약
 
-메모리: 71.8 MB, 시간: 0.37 ms
+메모리: 77.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 01일 10:56:58
+2024년 04월 01일 11:42:29
 
 ### 문제 설명
 
--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -4,35 +4,27 @@ class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
         int cnt = 0;
-        Arrays.sort(lottos);
-        Arrays.sort(win_nums);
-        // System.out.println(Arrays.toString(lottos));
-        // System.out.println(Arrays.toString(win_nums));
         
         for (int i = 0; i < 6; i++) {
             if (lottos[i] == 0) cnt++;
         }
-        // System.out.println(cnt);
             
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
-                if (lottos[i] != win_nums[j]) continue;
+                if (lottos[i] == win_nums[j]) {
                     answer[0]++;
                     answer[1]++;
+                }
             }
         }
         
-        answer[0] += cnt;
+        int best = 7 - cnt - answer[1];
+        int worst = 7 - answer[1];
         
-        for (int i = 0; i < 2; i++) {
-            if (answer[i] == 6) answer[i] = 1;
-            else if (answer[i] == 5) answer[i] = 2;
-            else if (answer[i] == 4) answer[i] = 3;
-            else if (answer[i] == 3) answer[i] = 4;
-            else if (answer[i] == 2) answer[i] = 5;
-            else answer[i] = 6;
-        }
+        if (best < 1) best = 1;
+        else if (best > 6) best = 6;
+        if (worst > 6) worst = 6;
         
-        return answer;
+        return new int[]{best, worst};
     }
 }

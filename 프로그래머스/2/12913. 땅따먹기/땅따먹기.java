1e005cccf32dedea609dbc9e21d330ddfd56f7dc
HSPU <95858935+HSPU@users.noreply.github.com>
2024-04-19 12:27:39 +0900

    [level 2] Title: 땅따먹기, Time: 19.91 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,18 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+
+        for(int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
+        }
+
+        for(int score : land[land.length - 1]){
+            answer = Math.max(answer, score);
+        }
+
+        return answer;
+    }
+}

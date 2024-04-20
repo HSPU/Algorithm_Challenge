462dfbd9ba7d8e8521086b29596802f2f3d6426d
HSPU <95858935+HSPU@users.noreply.github.com>
2024-04-20 12:03:33 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char[] arr = my_string.toCharArray();
+        arr[num1] = my_string.charAt(num2);
+        arr[num2] = my_string.charAt(num1);
+        return String.valueOf(arr);
+    }
+}

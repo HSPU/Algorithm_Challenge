2bcf3581dfb86eca720ae2c46c62a9b3a7a60d60
HSPU <95858935+HSPU@users.noreply.github.com>
2024-05-10 22:31:22 +0900

    [level 2] Title: 모음 사전, Time: 3.21 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,13 +1,26 @@
+import java.util.*;
+
 class Solution {
+    List<String> list = new ArrayList<>();
+    String[] words = {"A", "E", "I", "O", "U"};
     public int solution(String word) {
         int answer = 0;
-        int total = 3905;
-        String vowel = "AEIOU";
+        dfs("", 0);
         
-        for (String str : word.split("")) {
-            total /= 5;
-            answer += total * vowel.indexOf(str) + 1;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(word)) {
+                answer = i;
+                break;
+            }
         }
         return answer;
+    }
+    
+    void dfs(String str, int len) {
+        list.add(str);
+        if (len == 5) return;
+        for (int i = 0; i < 5; i++) {
+            dfs(str + words[i], len + 1);
+        }
     }
 }

62744f3b9ca9296ca8c0a4f806ce063cabd19b39
HSPU <95858935+HSPU@users.noreply.github.com>
2024-06-12 15:32:36 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.10 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,15 +1,15 @@
 class Solution {
     public long solution(String numbers) {
-        if (numbers.contains("zero")) numbers = numbers.replace("zero", "0");
-        if (numbers.contains("one")) numbers = numbers.replace("one", "1");
-        if (numbers.contains("two")) numbers = numbers.replace("two", "2");
-        if (numbers.contains("three")) numbers = numbers.replace("three", "3");
-        if (numbers.contains("four")) numbers = numbers.replace("four", "4");
-        if (numbers.contains("five")) numbers = numbers.replace("five", "5");
-        if (numbers.contains("six")) numbers = numbers.replace("six", "6");
-        if (numbers.contains("seven")) numbers = numbers.replace("seven", "7");
-        if (numbers.contains("eight")) numbers = numbers.replace("eight", "8");
-        if (numbers.contains("nine")) numbers = numbers.replace("nine", "9");
+        numbers = numbers.replace("zero", "0");
+        numbers = numbers.replace("one", "1");
+        numbers = numbers.replace("two", "2");
+        numbers = numbers.replace("three", "3");
+        numbers = numbers.replace("four", "4");
+        numbers = numbers.replace("five", "5");
+        numbers = numbers.replace("six", "6");
+        numbers = numbers.replace("seven", "7");
+        numbers = numbers.replace("eight", "8");
+        numbers = numbers.replace("nine", "9");
         
         return Long.valueOf(numbers);
     }

8c7c3780426be10c4b4c0bb9e69aacab828d822e
HSPU <95858935+HSPU@users.noreply.github.com>
2024-06-25 13:43:24 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int M, int N) {
-        if (M < N) return M - 1 + M * (N - 1);
-        else return N - 1 + N * (M - 1);
+        // if (M < N) return M - 1 + M * (N - 1);
+        // else return N - 1 + N * (M - 1);
+        return M * N - 1;
     }
 }

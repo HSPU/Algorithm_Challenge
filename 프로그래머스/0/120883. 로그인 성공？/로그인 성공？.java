9df7348de3b7cf1e96f0a2891eb86c5fcfa946a2
HSPU <95858935+HSPU@users.noreply.github.com>
2024-06-27 16:26:17 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        for (int i = 0; i < db.length; i++) {
+            if (id_pw[0].equals(db[i][0])) {
+                if (id_pw[1].equals(db[i][1])) answer = "login";
+                else answer = "wrong pw";
+            } else if (!id_pw[1].equals(db[i][1])) answer = "fail";
+        }
+        return answer;
+    }
+}

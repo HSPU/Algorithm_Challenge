39728e26a1f516069de4b935fe31b6bc774927bc
HSPU <95858935+HSPU@users.noreply.github.com>
2024-06-28 15:30:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,15 +1,9 @@
 class Solution {
     public int solution(String s) {
-        if (s.contains("zero")) s = s.replace("zero", "0");
-        if (s.contains("one")) s = s.replace("one", "1");
-        if (s.contains("two")) s = s.replace("two", "2");
-        if (s.contains("three")) s = s.replace("three", "3");
-        if (s.contains("four")) s = s.replace("four", "4");
-        if (s.contains("five")) s = s.replace("five", "5");
-        if (s.contains("six")) s = s.replace("six", "6");
-        if (s.contains("seven")) s = s.replace("seven", "7");
-        if (s.contains("eight")) s = s.replace("eight", "8");
-        if (s.contains("nine")) s = s.replace("nine", "9");
+        String[] str = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for (int i = 0; i < str.length; i++) {
+            if (s.contains(str[i])) s = s.replace(str[i], String.valueOf(i));
+        }
         return Integer.valueOf(s);
     }
 }

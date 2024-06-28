bebb0c738bfbabbb2469345a193d37ec1e5ee8ac
HSPU <95858935+HSPU@users.noreply.github.com>
2024-06-28 15:51:05 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.41 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] solution(int[][] arr) {
+        int[][] answer = {};
+        
+        if (arr.length >= arr[0].length) {
+            answer = new int[arr.length][arr.length];
+        } else {
+            answer = new int[arr[0].length][arr[0].length];
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                answer[i][j] = arr[i][j];
+            }
+        }
+        return answer;
+    }
+}
